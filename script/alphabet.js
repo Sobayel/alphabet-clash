f0754d240f5e64b8203bb1810070ea1c9b601717
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-02-14 15:04:22 +0600

    added random alphabet

---
+++ script/alphabet.js
@@ -0,0 +1,34 @@
+// function play(){
+    // step-1: hide the home screen to hide the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden') 
+    // console.log(homeSection.classList);
+
+
+
+    // step-2: show the playground
+//     const playgroundSection =document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+    // console.log(playgroundSection.classList);
+// }
+
+
+function continueGame(){
+    // step-1: generate a random alphabet
+    const alphabet = getRandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly alphabet to the screen alphabet (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+}
+
+ 
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
